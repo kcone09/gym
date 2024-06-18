1fcf9513dc2a6b7dcc3a0092175749fca8ff209a
kcone09 <kcone09@gmail.com>
2024-06-18 22:45:31 +0100

    correctedenvirinmemnt

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 }
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API,
 		'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
 	}
 };
